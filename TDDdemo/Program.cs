13b4a9a1a0fb30fb9eb51a531e3d55ec1248ac63
porter <wang_jingchao@pactera.com>
2013-02-19 15:31:19 +0800

    initialize project

---
+++ TDDdemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDDdemo
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //Sum(1, 2);
+
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        
+
+    }
+}
