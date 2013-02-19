0ac420e6625fd5015a227f0fb943ff730dbbc896
Porter <wjc1029@live.cn>
2013-02-19 16:01:32 +0800

    initialize TDD project

---
+++ TDDdemo/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDDdemo
+{
+    public  class Box
+    {
+        private Dictionary<string, int> tasks = new Dictionary<string, int>();
+
+        public void Push(string taskName, int priority)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Pop() {
+             throw new NotImplementedException();
+        }
+
+        public void List() {
+            throw new NotImplementedException();
+        }
+
+        public int Count() {
+            return this.tasks.Count;
+        }
+    }
+}
